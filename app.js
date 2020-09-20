c153401dde96a6162e678a7cf0cdf44fdf6f3da8
dgolaviya <54203124+dgolaviya@users.noreply.github.com>
2020-09-20 19:50:48 +0530

    corrected path.

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.post('webhook-checkout', express.raw(), webhookCheckout);
+app.post('/webhook-checkout', express.raw(), webhookCheckout);
 
 //Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
